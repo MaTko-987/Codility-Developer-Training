9ce566cda5f9ef2ff3ca690c5ab63448e505f990
Ma Tko <kwikzzstep@gmail.com>
2021-09-26 21:33:47 +0200

    1st commit

---
+++ cyclic-rotation.js
@@ -0,0 +1,15 @@
+function solution(A, K) {
+    
+
+     for (let i = 0; i < K; i++) {
+      A.unshift(A.pop());
+  }
+
+  return A;
+}
+
+
+console.log(solution([3,8,9,7,6], 1));
+console.log(solution([3,8,9,7,6], 2));
+console.log(solution([3,8,9,7,6], 3));
+
