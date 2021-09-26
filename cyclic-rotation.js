857d89fe97c76815b191c6dabdb60265b06bff70
Ma Tko <kwikzzstep@gmail.com>
2021-09-26 22:20:59 +0200

    4th commit

--- cyclic-rotation.js
+++ cyclic-rotation.js
@@ -2,7 +2,25 @@ function solution(A, K) {
     
 
      for (let i = 0; i < K; i++) {
-      A.unshift(A.pop());
+
+    /* 
+    for(statement1, statement2, statement3){}
+
+    Statement1 -> let i = 0 (sets a variable before the loop starts)
+    Statement2 -> defines the condition for the loop to run (i must be less than K)
+    Statement3 -> increases a value (i++) each time the code block in the loop has been executed.
+    */
+
+
+      A.unshift(A.pop());  
+      
+      /*
+
+      .unshift() - method adds new items to the beginning of an array, and returns the new length. Overwrites the original array. 
+      
+      .pop() - method removes the last element of an array. Returns the element it removes. Changes the length of the array.
+
+      */
   }
 
   return A;
